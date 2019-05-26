9d2d38eeb3b47a98518879346e9707683e2fb40e
Danylov <ol_danylov@ukr.net>
2019-05-26 13:43:10 +0300

    Add sevicea with entitya and serviceb with entityb.

--- gateway/src/main/webapp/app/entities/entity.module.ts
+++ gateway/src/main/webapp/app/entities/entity.module.ts
@@ -4,6 +4,14 @@ import { RouterModule } from '@angular/router';
 @NgModule({
   imports: [
     RouterModule.forChild([
+      {
+        path: 'entitya',
+        loadChildren: './servicea/entitya/entitya.module#ServiceaEntityaModule'
+      },
+      {
+        path: 'entityb',
+        loadChildren: './serviceb/entityb/entityb.module#ServicebEntitybModule'
+      }
       /* jhipster-needle-add-entity-route - JHipster will add entity modules routes here */
     ])
   ],
